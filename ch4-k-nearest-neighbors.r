8f4d2d4c719c3377ff378821d765b3cefa9ae98b
colson1111 <colson1111@gmail.com>
2015-08-08 13:54:06 -0400

    Create ch4-k-nearest-neighbors.r

---
+++ ch4-k-nearest-neighbors.r
@@ -0,0 +1,15 @@
+require(class)
+
+train.X=cbind(Lag1, Lag2)[train,]
+test.X=cbind(Lag1, Lag2)[!train,]
+train.Direction=Direction[train]
+
+set.seed(1)
+knn.pred=knn(train.X, test.X, train.Direction, k=1)
+
+table(knn.pred, Direction.2005)
+mean(knn.pred==Direction.2005)
+
+knn.pred=knn(train.X, test.X, train.Direction, k=3)
+table(knn.pred, Direction.2005)
+mean(knn.pred==Direction.2005)
