dca2edd5f49bffe0b45e0ce163fa95fc570ba903
colson1111 <colson1111@gmail.com>
2015-08-08 13:52:58 -0400

    Create ch4-quadratic-discriminant-analysis.r

---
+++ ch4-quadratic-discriminant-analysis.r
@@ -0,0 +1,13 @@
+#quadratic discriminant analysis
+
+qda.fit=qda(Direction~Lag1+Lag2, data=Smarket, subset=train)
+
+qda.fit
+
+qda.class=predict(qda.fit, Smarket.2005)$class
+
+table(qda.class, Direction.2005)
+
+mean(qda.class==Direction.2005)
+
+
